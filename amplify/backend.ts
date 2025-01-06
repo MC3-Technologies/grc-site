f503d161892ba3ce4ffc1ad8d4fa55fd7b308a23
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-06 13:59:46 -1000

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { chatGptFunction } from './functions/chat-gpt/resource';
+import { osintFunction } from './functions/osint/resrouce';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -10,4 +11,5 @@ defineBackend({
   auth,
   data,
   chatGptFunction,
+  osintFunction
 });
