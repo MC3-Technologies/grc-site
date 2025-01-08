325a155afb06a39e656dc2a11d5aa98d77e7bdb8
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-07 21:36:01 -1000

    Add terms of service sign up

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -235,6 +235,8 @@ export function Signup() {
                         <label
                           htmlFor="terms"
                           className="font-light text-gray-500 dark:text-gray-300"
+                          data-modal-target="default-modal"
+                          data-modal-toggle="default-modal"
                         >
                           I accept the{" "}
                           <a
@@ -244,6 +246,72 @@ export function Signup() {
                             Terms and Conditions
                           </a>
                         </label>
+                      </div>
+
+                      <div
+                        id="default-modal"
+                        tabIndex={-1}
+                        aria-hidden="true"
+                        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                      >
+                        <div className="relative p-4 w-full max-w-2xl max-h-full">
+                          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                Terms of Service
+                              </h3>
+                              <button
+                                type="button"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="default-modal"
+                              >
+                                <svg
+                                  className="w-3 h-3"
+                                  aria-hidden="true"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 14 14"
+                                >
+                                  <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                  />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                              </button>
+                            </div>
+                            <div className="p-4 md:p-5 space-y-4">
+                              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                By signing up for an account on MC3
+                                GRC, you acknowledge and agree that any
+                                company data you provide will be used solely for
+                                the purpose of assessing your organization's
+                                cybersecurity adherence. Our platform utilizes
+                                publicly available OSINT data, such as DNS
+                                records, server IPs, and email addresses, to
+                                enhance your cybersecurity awareness. We do not
+                                access, store, or process any private or
+                                non-publicly available information beyond what
+                                is voluntarily submitted through our forms.
+                              </p>
+                              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                MC3 Technologies is committed to protecting your
+                                privacy. All data collected remains strictly
+                                internal and is never shared, sold, or
+                                distributed to third parties. Your information
+                                is used exclusively for cybersecurity assessment
+                                and improvement, ensuring that our findings
+                                contribute to your organization's security
+                                posture. Additionally, you have full control
+                                over your account, with the option to delete or
+                                terminate it at any time.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                     <button
