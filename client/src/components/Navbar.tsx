357a41045465f393ecf51eebeef4306fa9eb3b9c
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-08 17:28:51 -1000

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -102,16 +102,24 @@ const Navbar = () => {
                       >
                         <li>
                           <a
-                            href="/account-details"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                            href="#"
+                            className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                           >
-                            Account Details
+                            My Account
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                          >
+                            My Assessments
                           </a>
                         </li>
 
                         <li>
                           <a
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                            className="block px-4 py-1 text-sm text-red-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-200 dark:hover:text-red"
                             style={{ cursor: "pointer" }}
                             onClick={() => {
                               signOutCurrentUser();
