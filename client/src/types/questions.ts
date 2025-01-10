2e2c3e5afb1f266fa5d1355e00f78f47cb459d7f
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-09 19:19:33 -1000

    Update questions.ts

--- client/src/types/questions.ts
+++ client/src/types/questions.ts
@@ -24,18 +24,14 @@ class QuestionNode {
   public id: string;
   public type: "radio" | "text" = "text";
   public question: string = "";
+  private options: string[] | null = null;
   public value: string = "";
   public conditionals: Conditional[] = [];
   public nextObjId: string | null = "";
   public next: QuestionNode | null = null;
-  private options: string[] | null = null;
 
   constructor(question: TextQuestion | RadioQuestion) {
     this.id = question.id;
-    this.question = question.question;
-    this.value = question.value;
-    this.conditionals = question.conditionals;
-    this.nextObjId = question.next;
     if (question.type === "radio") {
       this.type = "radio";
       question.options.forEach((option) => {
@@ -44,6 +40,10 @@ class QuestionNode {
         this.options = newOptions;
       });
     }
+    this.question = question.question;
+    this.value = question.value;
+    this.conditionals = question.conditionals;
+    this.nextObjId = question.next;
   }
 }
 
