be0f0900d37acee8cbba4624c1998e7257ef42b6
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-13 17:23:44 -1000

    New assessment class methods

--- client/src/types/Assesment.ts
+++ client/src/types/Assesment.ts
@@ -49,23 +49,46 @@ class Assessment {
     question.value = val;
   };
 
+  private getNextQuestion = (
+    id: string
+  ): TextQuestion | RadioQuestion | undefined => {
+    const nextQuestion = this.questions.find((q) => q.id === id);
+    return nextQuestion;
+  };
+
   public getQuestions = (): (TextQuestion | RadioQuestion)[] => {
+    // Set question array to return
     const ret: (TextQuestion | RadioQuestion)[] = [];
+
+    // Return ret if instance questions length is 0
     if (!this.questions.length) {
       return ret;
     }
-    let temp = this.questions[0];
+
+    // Set visited set to make sure no infinite loop in next references
     const visited = new Set<string>();
-    while (temp) {
-      if (visited.has(temp.id)) {
+    // Set temporary question node to start at
+    let curr = this.questions[0];
+
+    // Loop through question nodes next references and push to return array
+    while (curr) {
+      // If visited has current node id, throw loop detected error
+      if (visited.has(curr.id)) {
         throw new Error("Loop detected in questions flow");
       }
-      ret.push(temp);
-      visited.add(temp.id);
+
+      // Push current question node to curr
+      ret.push(curr);
+      // Add current node to visited set
+      visited.add(curr.id);
+
+      // Set next question to be found
       let nextQuestion: TextQuestion | RadioQuestion | undefined;
-      for (const conditional of temp.conditionals) {
-        if (temp.value === conditional.value) {
-          nextQuestion = this.questions.find((q) => q.id === conditional.next);
+
+      // Loop through current node conditionals and set nextQuestion to next node if conditional met
+      for (const conditional of curr.conditionals) {
+        if (curr.value === conditional.value) {
+          nextQuestion = this.getNextQuestion(conditional.next);
           if (!nextQuestion) {
             throw new Error(
               `Condition met, but next question ${conditional.next} does not exist`
@@ -74,16 +97,21 @@ class Assessment {
           break;
         }
       }
-      if (!nextQuestion && temp.next) {
-        nextQuestion = this.questions.find((q) => q.id === temp.next);
+
+      // If next question did not get defined by conditionals and current node still has next, find next question by id and set nextQuestion
+      if (!nextQuestion && curr.next) {
+        nextQuestion = this.getNextQuestion(curr.next);
         if (!nextQuestion) {
-          throw new Error(`Next question ${temp.next} does not exist`);
+          throw new Error(`Next question ${curr.next} does not exist`);
         }
       }
+
+      // If next question still not defined by above then break -- end of next references
       if (!nextQuestion) {
         break;
       }
-      temp = nextQuestion;
+      // Otherwise set current question node as next
+      curr = nextQuestion;
     }
     return ret;
   };
@@ -97,7 +125,7 @@ class Assessment {
   };
 }
 
-const getNewAssessment = () => {
+const getNewAssessment = (): Assessment => {
   return new Assessment(defaultQuestions, [], []);
 };
 
@@ -105,8 +133,19 @@ const getExistingAssessment = (
   questions: (TextQuestion | RadioQuestion)[],
   hardwareInventory: HardwareInventoryEntry[],
   softwareInventory: SoftwareInventoryEntry[]
-) => {
+): Assessment => {
   return new Assessment(questions, softwareInventory, hardwareInventory);
 };
 
-export { getNewAssessment, getExistingAssessment };
+const createNewAssessmentQuestions = (
+  questions: (TextQuestion | RadioQuestion)[]
+): Assessment => {
+  return new Assessment(questions, [], []);
+};
+
+export {
+  getNewAssessment,
+  getExistingAssessment,
+  createNewAssessmentQuestions,
+};
+export type { Assessment };
