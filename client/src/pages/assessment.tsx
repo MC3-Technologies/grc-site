aeb475fcdc16b278358687ff22a01d8b43d96d4f
Leighton Miguel <leim56@hawaii.rr.com>
2025-01-28 17:44:05 -1000

    Temporary hide assessment page

--- client/src/pages/assessment.tsx
+++ client/src/pages/assessment.tsx
@@ -1,4 +1,62 @@
-import { StrictMode, useEffect, useState } from "react";
+// import { StrictMode, useEffect, useState } from "react";
+// import { createRoot } from "react-dom/client";
+// import { initFlowbite } from "flowbite";
+
+// import "../index.css";
+// import "survey-core/defaultV2.min.css";
+
+// import Navbar from "../components/Navbar";
+// import Chat from "../components/Chat";
+// import Footer from "../components/Footer";
+
+// import { surveyJson } from "../assessmentDefaultQuestions";
+// import { Model, Survey } from "survey-react-ui";
+// import { BorderlessDark, BorderlessLight } from "survey-core/themes";
+
+// export function Assessment() {
+//   const [survey] = useState(new Model(surveyJson));
+
+//   useEffect(() => {
+//     initFlowbite();
+
+//     const storedDarkMode = localStorage.getItem("darkMode") === "true";
+//     if (storedDarkMode) {
+//       survey.applyTheme(BorderlessDark);
+//     } else {
+//       survey.applyTheme(BorderlessLight);
+//     }
+
+//     const handleDarkMode = () => survey.applyTheme(BorderlessDark);
+//     const handleLightMode = () => survey.applyTheme(BorderlessLight);
+
+//     window.addEventListener("darkMode", handleDarkMode);
+//     window.addEventListener("lightMode", handleLightMode);
+
+//     return () => {
+//       window.removeEventListener("darkMode", handleDarkMode);
+//       window.removeEventListener("lightMode", handleLightMode);
+//     };
+//   }, [survey]);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="mt-20 bg-white dark:bg-gray-900">
+//         <Survey model={survey} />
+//       </section>
+//       <Chat />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <Assessment />
+//   </StrictMode>
+// );
+
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { initFlowbite } from "flowbite";
 
@@ -9,40 +67,16 @@ import Navbar from "../components/Navbar";
 import Chat from "../components/Chat";
 import Footer from "../components/Footer";
 
-import { surveyJson } from "../assessmentDefaultQuestions";
-import { Model, Survey } from "survey-react-ui";
-import { BorderlessDark, BorderlessLight } from "survey-core/themes";
-
 export function Assessment() {
-  const [survey] = useState(new Model(surveyJson));
-
   useEffect(() => {
     initFlowbite();
-
-    const storedDarkMode = localStorage.getItem("darkMode") === "true";
-    if (storedDarkMode) {
-      survey.applyTheme(BorderlessDark);
-    } else {
-      survey.applyTheme(BorderlessLight);
-    }
-
-    const handleDarkMode = () => survey.applyTheme(BorderlessDark);
-    const handleLightMode = () => survey.applyTheme(BorderlessLight);
-
-    window.addEventListener("darkMode", handleDarkMode);
-    window.addEventListener("lightMode", handleLightMode);
-
-    return () => {
-      window.removeEventListener("darkMode", handleDarkMode);
-      window.removeEventListener("lightMode", handleLightMode);
-    };
-  }, [survey]);
+  }, []);
 
   return (
     <>
       <Navbar />
       <section className="mt-20 bg-white dark:bg-gray-900">
-        <Survey model={survey} />
+        Assessment page
       </section>
       <Chat />
       <Footer />
