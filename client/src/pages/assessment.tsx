65133d536c2a5aefa515e976c9f1f8bcdb74e28b
Leighton Miguel <leim56@hawaii.rr.com>
2025-03-10 15:17:52 -1000

    Update assessment.tsx

--- client/src/pages/assessment.tsx
+++ client/src/pages/assessment.tsx
@@ -12,22 +12,47 @@ import Footer from "../components/Footer";
 import { surveyJson } from "../assessmentQuestions";
 import { Model, Survey } from "survey-react-ui";
 import { BorderlessDark, BorderlessLight } from "survey-core/themes";
+import { getCurrentUser } from "../amplify/auth";
 
 export function Assessment() {
-  const [survey] = useState(new Model(surveyJson));
+  const [assessment, setAssessment] = useState<Model>(new Model(surveyJson));
 
+  // useEffect to handle assessment setting up and adding on completion handler
+  useEffect(() => {
+    const initialize = async () => {
+      // New assessment instance
+      const assessment = new Model(surveyJson);
+
+      // Get current user and associate user id with assessment
+      const user = await getCurrentUser();
+      assessment.setValue("userId", user.userId);
+
+      // Add assessment completion handler
+      assessment.onComplete.add((assessment) => {
+        // Handle assessment completion here by accessing assessment completion JSON with assessment.data
+        console.log(assessment.data);
+      });
+
+      // Set assessment state
+      setAssessment(assessment);
+    };
+
+    initialize();
+  }, []);
+
+  // useEffect to handle theme changes
   useEffect(() => {
     initFlowbite();
 
     const storedDarkMode = localStorage.getItem("darkMode") === "true";
     if (storedDarkMode) {
-      survey.applyTheme(BorderlessDark);
+      assessment.applyTheme(BorderlessDark);
     } else {
-      survey.applyTheme(BorderlessLight);
+      assessment.applyTheme(BorderlessLight);
     }
 
-    const handleDarkMode = () => survey.applyTheme(BorderlessDark);
-    const handleLightMode = () => survey.applyTheme(BorderlessLight);
+    const handleDarkMode = () => assessment.applyTheme(BorderlessDark);
+    const handleLightMode = () => assessment.applyTheme(BorderlessLight);
 
     window.addEventListener("darkMode", handleDarkMode);
     window.addEventListener("lightMode", handleLightMode);
@@ -36,13 +61,13 @@ export function Assessment() {
       window.removeEventListener("darkMode", handleDarkMode);
       window.removeEventListener("lightMode", handleLightMode);
     };
-  }, [survey]);
+  }, [assessment]);
 
   return (
     <>
       <Navbar />
       <section className="mt-20 bg-white dark:bg-gray-900">
-        <Survey model={survey} />
+        <Survey model={assessment} />
       </section>
       <Chat />
       <Footer />
@@ -55,37 +80,3 @@ createRoot(document.getElementById("root")!).render(
     <Assessment />
   </StrictMode>
 );
-
-// import { StrictMode, useEffect } from "react";
-// import { createRoot } from "react-dom/client";
-// import { initFlowbite } from "flowbite";
-
-// import "../index.css";
-// import "survey-core/defaultV2.min.css";
-
-// import Navbar from "../components/Navbar";
-// import Chat from "../components/Chat";
-// import Footer from "../components/Footer";
-
-// export function Assessment() {
-//   useEffect(() => {
-//     initFlowbite();
-//   }, []);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <section className="mt-20 bg-white dark:bg-gray-900">
-//         Assessment page
-//       </section>
-//       <Chat />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <Assessment />
-//   </StrictMode>,
-// );
