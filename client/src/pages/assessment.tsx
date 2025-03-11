fc6cc5004bd3e8d4fd3d15c1204eddfdf997581a
Leighton Miguel <leim56@hawaii.rr.com>
2025-03-10 15:03:40 -1000

    Assessment questions

--- client/src/pages/assessment.tsx
+++ client/src/pages/assessment.tsx
@@ -1,62 +1,4 @@
-// import { StrictMode, useEffect, useState } from "react";
-// import { createRoot } from "react-dom/client";
-// import { initFlowbite } from "flowbite";
-
-// import "../index.css";
-// import "survey-core/defaultV2.min.css";
-
-// import Navbar from "../components/Navbar";
-// import Chat from "../components/Chat";
-// import Footer from "../components/Footer";
-
-// import { surveyJson } from "../assessmentDefaultQuestions";
-// import { Model, Survey } from "survey-react-ui";
-// import { BorderlessDark, BorderlessLight } from "survey-core/themes";
-
-// export function Assessment() {
-//   const [survey] = useState(new Model(surveyJson));
-
-//   useEffect(() => {
-//     initFlowbite();
-
-//     const storedDarkMode = localStorage.getItem("darkMode") === "true";
-//     if (storedDarkMode) {
-//       survey.applyTheme(BorderlessDark);
-//     } else {
-//       survey.applyTheme(BorderlessLight);
-//     }
-
-//     const handleDarkMode = () => survey.applyTheme(BorderlessDark);
-//     const handleLightMode = () => survey.applyTheme(BorderlessLight);
-
-//     window.addEventListener("darkMode", handleDarkMode);
-//     window.addEventListener("lightMode", handleLightMode);
-
-//     return () => {
-//       window.removeEventListener("darkMode", handleDarkMode);
-//       window.removeEventListener("lightMode", handleLightMode);
-//     };
-//   }, [survey]);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <section className="mt-20 bg-white dark:bg-gray-900">
-//         <Survey model={survey} />
-//       </section>
-//       <Chat />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <Assessment />
-//   </StrictMode>
-// );
-
-import { StrictMode, useEffect } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { initFlowbite } from "flowbite";
 
@@ -67,16 +9,40 @@ import Navbar from "../components/Navbar";
 import Chat from "../components/Chat";
 import Footer from "../components/Footer";
 
+import { surveyJson } from "../assessmentQuestions";
+import { Model, Survey } from "survey-react-ui";
+import { BorderlessDark, BorderlessLight } from "survey-core/themes";
+
 export function Assessment() {
+  const [survey] = useState(new Model(surveyJson));
+
   useEffect(() => {
     initFlowbite();
-  }, []);
+
+    const storedDarkMode = localStorage.getItem("darkMode") === "true";
+    if (storedDarkMode) {
+      survey.applyTheme(BorderlessDark);
+    } else {
+      survey.applyTheme(BorderlessLight);
+    }
+
+    const handleDarkMode = () => survey.applyTheme(BorderlessDark);
+    const handleLightMode = () => survey.applyTheme(BorderlessLight);
+
+    window.addEventListener("darkMode", handleDarkMode);
+    window.addEventListener("lightMode", handleLightMode);
+
+    return () => {
+      window.removeEventListener("darkMode", handleDarkMode);
+      window.removeEventListener("lightMode", handleLightMode);
+    };
+  }, [survey]);
 
   return (
     <>
       <Navbar />
       <section className="mt-20 bg-white dark:bg-gray-900">
-        Assessment page
+        <Survey model={survey} />
       </section>
       <Chat />
       <Footer />
@@ -87,5 +53,39 @@ export function Assessment() {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Assessment />
-  </StrictMode>,
+  </StrictMode>
 );
+
+// import { StrictMode, useEffect } from "react";
+// import { createRoot } from "react-dom/client";
+// import { initFlowbite } from "flowbite";
+
+// import "../index.css";
+// import "survey-core/defaultV2.min.css";
+
+// import Navbar from "../components/Navbar";
+// import Chat from "../components/Chat";
+// import Footer from "../components/Footer";
+
+// export function Assessment() {
+//   useEffect(() => {
+//     initFlowbite();
+//   }, []);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="mt-20 bg-white dark:bg-gray-900">
+//         Assessment page
+//       </section>
+//       <Chat />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <Assessment />
+//   </StrictMode>,
+// );
