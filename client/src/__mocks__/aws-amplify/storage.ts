62a69c7a4de31277e5bc892ac9b917d48fd1298a
Ibrahim Matar <imatar77@hawaii.edu>
2025-03-18 18:01:51 -1000

    fix: lint errros

--- client/src/__mocks__/aws-amplify/storage.ts
+++ client/src/__mocks__/aws-amplify/storage.ts
@@ -45,7 +45,7 @@ export const __resetMockStorage = () => {
 };
 
 // Utility to pre-populate mock storage with test data
-export const __setMockStorageItem = (path: string, data: any) => {
+export const __setMockStorageItem = (path: string, data: unknown) => {
   const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
   mockStorageData.set(path, blob);
 }; 
